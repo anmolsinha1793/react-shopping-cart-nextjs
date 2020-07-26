5b26fd94d5e4cb2850b9fa5c73fd356d85d8f063
anmolsinha1793 <anmol.sinha1993@gmail.com>
2020-07-27 03:44:25 +0530

    implemented pagination

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import axios from 'axios';
 import ProductList from '../components/Index/ProductList';
 import baseUrl from '../utils/baseUrl';
+import ProductPagination from '../components/Index/ProductPagination';
 
-function Home({ products }) {
-  return <ProductList products={products}/>;
+function Home({ products, totalPages }) {
+  return <><ProductList products={products}/>
+    <ProductPagination totalPages={totalPages}/></>;
 }
 
-Home.getInitialProps = async() => {
+Home.getInitialProps = async(ctx) => {
+  console.log(ctx.query);
+  const page = ctx.query.page ? ctx.query.page : '1';
+  //* number of products to show per page
+  const size = 4;
+  const payload = {params: { page, size }};
   const url = `${baseUrl}/api/products`;
-  const response = await axios.get(url);
-  return { products: response.data }
+  //* fetch data from server
+  const response = await axios.get(url, payload);
+  //* return response data as an object
+  return response.data;
 }
 export default Home;
