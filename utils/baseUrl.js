81d310f9ffd33c92987068cf12c61827098e38c3
anmolsinha1793 <anmol.sinha1993@gmail.com>
2020-10-06 13:06:28 +0530

    made changes for deployment url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = process.env.NODE_ENV === "production" ? "https://deployment-url.now.sh" : "http://localhost:3000";
+const baseUrl = process.env.NODE_ENV === "production" ? "https://react-shopping-cart-adi.vercel.app" : "http://localhost:3000";
 
 export default baseUrl;
