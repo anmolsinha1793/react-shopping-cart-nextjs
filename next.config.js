2a766cf59e0549ec4dbeca1bf587c86181504099
anmolsinha1793 <anmol.sinha1993@gmail.com>
2021-02-07 14:56:25 +0530

    made changes for docker cors issue

--- next.config.js
+++ next.config.js
@@ -5,5 +5,17 @@ module.exports = {
     JWT_SECRET: "rhjhdjjsrbe33k4",
     CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/adi17/image/upload",
     STRIPE_SECRET_KEY: "sk_test_wf2ZhiLmctf34whJ1W0yhR0F00YlOuF6HI"
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" }
+        ]
+      }
+    ]
   }
 };
